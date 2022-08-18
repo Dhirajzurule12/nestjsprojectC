c89318e809f0567a6193899cf38a97ab08322235
Dhirajzurule12 <zuruledhiraj@gmail.com>
2022-08-18 14:30:43 +0530

    updated put method

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('nestjs');
+  //app.setGlobalPrefix('nestjs');
   await app.listen(3000);
 }
 bootstrap();
