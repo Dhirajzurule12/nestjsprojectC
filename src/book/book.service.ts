233df87ab4b92ed19f575ad031cead021d3d4481
mayur.wakikar <mayurwakikar17@gmail.com>
2022-08-18 12:06:34 +0530

    feat/booklib1: added changes

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -7,10 +7,10 @@ import { Book } from './entities/book.interface';
 
 @Injectable()
 export class BookService {
-    constructor (
+    constructor(
         @InjectRepository(BookEntity)
         private readonly bookRepository: Repository<BookEntity>
-    ) {}
+    ) { }
 
     create(book: Book): Observable<Book> {
         return from(this.bookRepository.save(book));
@@ -19,23 +19,23 @@ export class BookService {
     findAllBooks(): Observable<Book[]> {
         return from(this.bookRepository.find());
     }
-    getOne(id:number):Observable<Book> {
-        const book_id=id;
-        return from(this.bookRepository.findOneBy({book_id}))
+    getOne(id: number): Observable<Book> {
+        const book_id = id;
+        return from(this.bookRepository.findOneBy({ book_id }))
 
     }
     // getSpecificBook(id:number): Observable<Book> {
-        // return from(this.bookRepository.find(id));
-        // const book_id = id;
-        // return from(this.bookRepository.findOneBy({book_id}));
+    // return from(this.bookRepository.find(id));
+    // const book_id = id;
+    // return from(this.bookRepository.findOneBy({book_id}));
     // }
 
-    updatePutBook(id: number, book: Book): Observable<UpdateResult>{
-        return from(this.bookRepository.update(id,book));
+    updatePutBook(id: number, book: Book): Observable<UpdateResult> {
+        return from(this.bookRepository.update(id, book));
     }
 
-    updatePatchBook(id: number, book: Book): Observable<UpdateResult>{
-        return from(this.bookRepository.update(id,book));
+    updatePatchBook(id: number, book: Book): Observable<UpdateResult> {
+        return from(this.bookRepository.update(id, book));
     }
 
     deleteBook(id: number): Observable<DeleteResult> {
