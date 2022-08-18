7701ec8c97196ff47fd436e93786bbc558bda623
mayur.wakikar <mayurwakikar17@gmail.com>
2022-08-18 14:25:24 +0530

    coomited usually

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -30,7 +30,9 @@ export class BookService {
     // return from(this.bookRepository.findOneBy({book_id}));
     // }
 
-
+    updatePutBook(id: number, book: Book): Observable<UpdateResult> {
+        return from(this.bookRepository.update(id, book));
+    }
 
     updatePatchBook(id: number, book: Book): Observable<UpdateResult> {
         return from(this.bookRepository.update(id, book));
