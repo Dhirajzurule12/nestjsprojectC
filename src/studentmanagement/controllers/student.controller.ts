a17d97b5b117597891e19a633bd489d933ec74bf
Dhirajzurule12 <zuruledhiraj@gmail.com>
2022-08-19 16:00:44 +0530

    feat: added image folder

--- src/studentmanagement/controllers/student.controller.ts
+++ src/studentmanagement/controllers/student.controller.ts
@@ -7,13 +7,20 @@ import {
   Param,
   Post,
   Put,
+  Res,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 import { StudentPost } from '../model/post.interface';
 import { StudentsService } from '../Services/student.service';
 import { Observable } from 'rxjs';
 import { UpdateResult, DeleteResult } from 'typeorm';
+import { diskStorage } from 'multer';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { extname } from 'path';
 @Controller('student')
 export class StudentsController {
+  imagepath: string;
   constructor(private studentService: StudentsService) {}
 
   @Post()
@@ -28,9 +35,7 @@ export class StudentsController {
 
   @Put(':id')
   update(
-    @Param('id',
-   
-    ) id: number,
+    @Param('id') id: number,
     @Body()
     studentPost: StudentPost,
   ): Observable<UpdateResult> {
@@ -40,5 +45,32 @@ export class StudentsController {
   @Delete(':id')
   delete(@Param('id') id: number): Observable<DeleteResult> {
     return this.studentService.deletePost(id);
+  }
+
+  @Post('image')
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './images',
+        filename: (req, image, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(image.originalname);
+          // const filename = `${image.originalname}-${uniqueSuffix}${ext}`;
+          const filename = `${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+  )
+  handleupload(@UploadedFile() image: Express.Multer.File) {
+    this.imagepath = image.path;
+    console.log('image', image);
+    console.log('path', image.path);
+    return 'file upload API';
+  }
+  @Get('/image/:image')
+  seeUploadedFile(@Param('image') image, @Res() res) {
+    return res.sendFile(image, { root: './images' });
   }
 }
