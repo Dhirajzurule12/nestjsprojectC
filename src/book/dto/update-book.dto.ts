1cd61d117ee87cbb3fe35fb20c190d238096c750
mayur.wakikar <mayurwakikar17@gmail.com>
2022-08-23 13:11:10 +0530

    feat ready to submit

--- src/book/dto/update-book.dto.ts
+++ src/book/dto/update-book.dto.ts
@@ -4,8 +4,8 @@ import { CreateBookDto } from './create-book.dto';
 export class UpdateBookDto extends PartialType(CreateBookDto) {
         book_id: number;
         book_name: string;
-        author: string;
-        price: number;
+        book_author: string;
+        book_price: number;
         book_image: string;
         book_isbn: string;
     
